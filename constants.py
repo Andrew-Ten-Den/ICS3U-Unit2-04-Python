ef829da70bb344cf561694d8548c01f9224f0071
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-07 16:13:29 +0000

    added in pizza_calculation

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew Ten-Den
+# Created on: March 2022
+# This module contains constants for the pizza_calculation program
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
